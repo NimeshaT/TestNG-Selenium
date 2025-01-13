a8e713262c8573b87c0705b1c86f493f56c4e466
NimeshaT <nimeshaijse2020@gmail.com>
2025-01-13 22:40:19 +0530

    create test cases and save screenshots

--- src/test/java/Base/BaseTest.java
+++ src/test/java/Base/BaseTest.java
@@ -1,5 +1,6 @@
 package Base;
 
+import Utils.TakeErrorScreenShots;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
